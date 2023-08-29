29940a4653f14f8a106aa4e59ae8238d903c2c77
Elvis Mack Jr <>
2023-08-28 22:26:28 -0400

    Day 2 - Project 2 Import Test

---
+++ Day 2/Day 2 - Project 2/Day 2 - Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day 2 - Project 2
+//
+//  Created by Consultant on 8/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
