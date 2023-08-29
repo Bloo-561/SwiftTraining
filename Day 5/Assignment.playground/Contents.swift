d6b6b3f28280cc87eebff2e968bca4631036f4d2
Elvis Mack Jr <>
2023-08-29 14:59:37 -0400

    Day 5 - Init Commit Part 2

---
+++ Day 5/Assignment.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+
+// Theory
+
+// 1. What is protocol delegate pattern, write some samle code to explin?
+
+// 2. Steps to create tableView
+
+// 3. What is use of storyboard refrence?
+
+// 4. What is segue?
+
+
+// Practical
+
+
+// 1. Create a talbeview and display list of 10 students using array
+
+    // Student should have name , email , dob, avtar
+    // use avtar image from assets
+
+
+
+// 2. Write logic for calculator UI.
+
+
+// 3. from Loginview controller navigate to Student list .
