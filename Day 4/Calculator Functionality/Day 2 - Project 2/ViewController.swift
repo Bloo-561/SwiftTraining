7d42adc323d04666a57477ec0a2e6d493106cfee
Elvis Mack Jr <>
2023-08-29 23:47:49 -0400

    End of Day Push - Day 5

--- Day 4/Calculator Functionality/Day 2 - Project 2/ViewController.swift
+++ Day 4/Calculator Functionality/Day 2 - Project 2/ViewController.swift
@@ -9,11 +9,119 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var calcProblem: UILabel!
+    @IBOutlet var calcAnswer: UILabel!
+    
+    var result : String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        clearAll()
         
     }
-
-
+    
+    func clearAll() {
+        result = ""
+        calcProblem.text = ""
+        calcAnswer.text = ""
+    }
+    
+    // Clear Buttons
+    
+    @IBAction func allClear(_ sender: Any) {
+        clearAll()
+    }
+    @IBAction func backClear(_ sender: Any) {
+        if(!result.isEmpty) {
+            result.removeLast()
+            calcProblem.text = result
+        }
+    }
+    
+    func math(value : String) {
+        result += value
+        calcProblem.text = result
+    }
+    
+    // Special Buttons
+    
+    @IBAction func squareRoot(_ sender: Any) {
+        math(value: "√")
+    }
+    @IBAction func pi(_ sender: Any) {
+        math(value: "π")
+    }
+    @IBAction func exponent(_ sender: Any) {
+        math(value: "^")
+    }
+    @IBAction func factorial(_ sender: Any) {
+        math(value: "!")
+    }
+    @IBAction func par(_ sender: Any) {
+        math(value: "%")
+    }
+    @IBAction func percentage(_ sender: Any) {
+        math(value: "%")
+    }
+    
+    // Basic Functions
+    @IBAction func add(_ sender: Any) {
+        math(value: "+")
+    }
+    @IBAction func sub(_ sender: Any) {
+        math(value: "-")
+    }
+    @IBAction func multipy(_ sender: Any) {
+        math(value: "x")
+    }
+    @IBAction func divide(_ sender: Any) {
+        math(value: "÷")
+    }
+    @IBAction func decimal(_ sender: Any) {
+        math(value: ".")
+    }
+    @IBAction func equal(_ sender: Any) {
+        let expression = NSExpression(format: result)
+        let solution = expression.expressionValue(with: 0, context: nil) as! Double
+        calcAnswer.text = String(solution)
+    }
+    
+    // Number Functions
+    @IBAction func zero(_ sender: Any) {
+        math(value: "0")
+    }
+    @IBAction func one(_ sender: Any) {
+        math(value: "1")
+    }
+    @IBAction func two(_ sender: Any) {
+        math(value: "2")
+    }
+    @IBAction func three(_ sender: Any) {
+        math(value: "3")
+    }
+    @IBAction func four(_ sender: Any) {
+        math(value: "4")
+    }
+    @IBAction func five(_ sender: Any) {
+        math(value: "5")
+    }
+    @IBAction func six(_ sender: Any) {
+        math(value: "6")
+    }
+    @IBAction func seven(_ sender: Any) {
+        math(value: "7")
+    }
+    @IBAction func eight(_ sender: Any) {
+        math(value: "8")
+    }
+    @IBAction func nine(_ sender: Any) {
+        math(value: "9")
+    }
+    
+    
+    
+    
 }
+
 
--- Day 5/Assignment.playground/Contents.swift
+++ Day 5/Assignment.playground/Contents.swift
@@ -4,18 +4,29 @@ import UIKit
 // Theory
 
 // 1. What is protocol delegate pattern, write some samle code to explin?
+    // Answer: 
 
 // 2. Steps to create tableView
+    // Answer:
+        // Step 1 : Create a class controller
+        // Step 2 : Create a tableView Class
+        // Step 3 : Create a object within the class controller to represent the table
+        // Step 4 : Declare the object as a type controller delegate
+        // Step 5 : Pass the object that delegate reference
+        // Step 6 : Create a protocol tableview datasource with two methods, a method for the number of rows and a method for the rows at index
+        // Step 7 : In table view make controller object with datasource protocol
 
 // 3. What is use of storyboard refrence?
+    // Answer:
 
 // 4. What is segue?
+    // Answer: A segue is the transition between two view controllers in a storyboard file and is represented by visual connectors
 
 
 // Practical
 
 
-// 1. Create a talbeview and display list of 10 students using array
+// 1. Create a tableview and display list of 10 students using array
 
     // Student should have name , email , dob, avtar
     // use avtar image from assets
