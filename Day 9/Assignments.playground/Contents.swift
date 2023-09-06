fe10d1e20c08b54579b25bef1f3cc2574eb640a1
Elvis Mack Jr <>
2023-09-06 08:15:21 -0400

    Day 9 - Assignment Complete

--- Day 9/Assignments.playground/Contents.swift
+++ Day 9/Assignments.playground/Contents.swift
@@ -17,10 +17,14 @@ var greeting = "Hello, playground"
 
 
 
-// what is diff b/w escaping and non escaping
-// what is associated values
-// what is raw values
-// what is Generics
+// What is diff b/w escaping and non escaping
+    // Answer: Escaping leaves the scope that it is passed to and keeps the memory to be used elsewhere. Meanwhile, Non-escaping does not leave the slope and leaves once the function is executed.
 
+// What is associated values
+    // Answer: Associated Values are a way to attach additional information to enum values without defining them as any type
 
+// What is raw values
+    // Answer: Raw Values attach additional information within a time-set value. Each value must be unique in a enem declaration
 
+// What is Generics
+    // Answer: Generics allow to create various classes and functions that could be used with different data types.
