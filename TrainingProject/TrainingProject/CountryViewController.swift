99eda94f9361432d8bcb40e5703532796fbaa3f1
Elvis Mack Jr <>
2023-09-06 09:56:33 -0400

    Day 9 - Networking

--- TrainingProject/TrainingProject/CountryViewController.swift
+++ TrainingProject/TrainingProject/CountryViewController.swift
@@ -7,17 +7,35 @@
 
 import UIKit
 
-class CountryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
+protocol NetworkAction {
+    func getData(countries: [Country]?, error: Error?)
+}
+
+class CountryViewController: UIViewController, UITableViewDataSource, NetworkAction {
+     
     @IBOutlet weak var tableView: UITableView!
     
     var countries : [Country]?
+    let networkProtocol = NetworkProtocol()
+    let networkClosure = NetworkClosure()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        getCountry()
+        networkClosure.getCountry { countries, error in
+            self.countries = countries
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        
         tableView.dataSource = self
-        tableView.delegate = self
+        networkProtocol.getCountry()
+    }
+    
+    func getData(countries: [Country]?, error: Error?) {
+        
+        self.countries = countries
+        self.tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -39,37 +57,12 @@ class CountryViewController: UIViewController, UITableViewDataSource, UITableVie
         let currency = countries[indexPath.row].currency.name
         let currencySymbol = countries[indexPath.row].currency.symbol
         
-        cell?.country.text = "Region: \(region) | \(code) \(name)"
+        cell?.region.text = "\(region) | \(code)"
+        cell?.country.text = "\(name)"
         cell?.capital.text = "Country Capital: \(capital)"
         cell?.language.text = "Country National Language: \(language)"
         cell?.currency.text = "Country Currency: \(currencySymbol ?? "") \(currency)"
         
         return cell ?? UITableViewCell()
-    }
-    
-    func getCountry() {
-        
-        guard let url = URL(string: "https://gist.githubusercontent.com/peymano-wmt/32dcb892b06648910ddd40406e37fdab/raw/db25946fd77c5873b0303b858e861ce724e0dcd0/countries.json") else { return }
-        
-        let urlRequest = URLRequest(url: url)
-        
-        let urlSession = URLSession(configuration: URLSessionConfiguration.default)
-                
-                let dataTask = urlSession.dataTask(with: urlRequest) { data, urlResponse, error in
-                    
-                    guard let data = data else { return }
-                    
-                    let decoder = JSONDecoder()
-                    do {
-                        self.countries = try decoder.decode([Country].self, from: data)
-                        DispatchQueue.main.async {
-                            self.tableView.reloadData()
-                        }
-
-                    } catch {
-                        print(error)
-                    }
-                }
-                dataTask.resume()
     }
 }
